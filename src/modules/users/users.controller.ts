56b2ac5dca5b4f2c748702a0b420bbaeea6cbde4
brunoc-cardoso <brunocpessoal@gmail.com>
2023-07-24 19:57:22 -0300

    feat: add ActiveUserId custom decorator to get userId from request

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Get, Req } from '@nestjs/common';
 
 import { UsersService } from '_modules/users/users.service';
+import { ActiveUserId } from '_shared/decorators/ActiveUserId';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get('/me')
-  me(@Req() request: any) {
-    return this.usersService.getUserById(request.userId);
+  me(@ActiveUserId() userId: string) {
+    return this.usersService.getUserById(userId);
   }
 }
