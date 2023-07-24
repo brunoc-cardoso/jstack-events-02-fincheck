964b0db7e8812a02832e0a64332c53b389ae4777
brunoc-cardoso <brunocpessoal@gmail.com>
2023-07-24 19:30:42 -0300

    feat: add support for public routes within auth guard

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, SetMetadata } from '@nestjs/common';
 
 import { AuthService } from '_modules/auth/auth.service';
 import { SigninDto } from '_modules/auth/dto/signin';
@@ -9,12 +9,14 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signin')
+  @SetMetadata('IS_PUBLIC', true)
   authenticate(@Body() signin: SigninDto) {
     return this.authService.signin(signin);
   }
 
   @Post('signup')
-  create(@Body() signupDto: SignupDto) {
+  @SetMetadata('IS_PUBLIC', true)
+  signup(@Body() signupDto: SignupDto) {
     return this.authService.signup(signupDto);
   }
 }
